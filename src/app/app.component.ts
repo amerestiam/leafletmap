b5a20d959fa2a3d61f5877c8117bff0f7c33a407
Labidi Najdi <labidi.najdi@gmail.com>
2022-02-23 00:10:35 -1100

    front end commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import {Map, tileLayer , marker} from 'leaflet';
+
 
 @Component({
   selector: 'app-root',
@@ -7,4 +9,21 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'leafletmap';
+
+  ngAfterViewInit(): void {
+    
+    const map = new Map ("map").setView([48.87045, 2.37273],13);
+
+   tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+	maxZoom: 19,
+	attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+}).addTo(map);
+
+const markeItem = marker([48.87045, 2.37273]).addTo(map);
+
+map.fitBounds ([
+  [markeItem.getLatLng().lat,markeItem.getLatLng().lng]
+]);
+
+  }
 }
